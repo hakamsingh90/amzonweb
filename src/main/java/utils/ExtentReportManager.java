523ba47b9550562b4679ed8ef03e1398091989bb
Hakam Singh <112749523+hakamsingh90@users.noreply.github.com>
2022-10-08 18:09:09 +0530

    add base util and report classes

--- src/main/java/utils/ExtentReportManager.java
+++ src/main/java/utils/ExtentReportManager.java
@@ -1,4 +1,33 @@
 package utils;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+
+
 public class ExtentReportManager {
+
+
+    public static ExtentReports report;
+
+    public static ExtentReports getReportInstance(){
+
+        if(report == null){
+            String reportName = DateUtil.getTimeStamp() + ".html";
+            ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/" + reportName);
+            report =  new ExtentReports();
+            report.attachReporter(htmlReporter);
+
+            report.setSystemInfo("OS", "macOS");
+            report.setSystemInfo("Environment", "local");
+            report.setSystemInfo("Browser", "Chrome");
+
+            htmlReporter.config().setDocumentTitle("Local amazon page result");
+            htmlReporter.config().setReportName("UI Test Report");
+            htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+            htmlReporter.config().setTimeStampFormat("MMM dd, yyyy HH:mm:ss");
+        }
+
+        return report;
+    }
 }
